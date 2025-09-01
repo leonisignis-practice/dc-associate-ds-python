bcd84caaba36b178ffb1c77c97e3711c3826f183
leonisignis <leonisignisxii@gmail.com>
2025-09-01 01:40:06 -0500

    completed: variables and types

---
+++ 01-introduction-to-python/01-python-basics/04-variables-and-types.py
@@ -0,0 +1,27 @@
+# Calculate Body Mass Index (BMI)
+height = 1.70
+weight = 75
+bmi = weight / height**2
+print(bmi)
+
+# Types
+print(type(bmi))
+print(type(height))
+print(type(weight))
+
+# Convert float to integer
+print(int(weight))
+print(int(height))
+print(int(bmi))
+
+x = "bmi"
+y = "bmi"
+print(type(x))
+print(type(y))
+
+z = True
+print(type(z))
+
+print(2 + 3)
+print("ab" + "cd")
+print("ab" * 3)
