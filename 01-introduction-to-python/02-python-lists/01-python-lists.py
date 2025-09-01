3a8bdfd396cde51615fc16961d191d96d83e9e89
leonisignis <leonisignisxii@gmail.com>
2025-09-01 02:10:27 -0500

    completed: python lists

---
+++ 01-introduction-to-python/02-python-lists/01-python-lists.py
@@ -0,0 +1,22 @@
+height1 = 1.73
+height2 = 1.68
+height3 = 1.71
+height4 = 1.89
+
+# This is inconvenient
+# We can use a list instead
+family = [1.73, 1.68, 1.71, 1.89]
+
+# A list can contain different types
+family = ["liz", 1.73, "emma", 1.68, "mom", 1.71, "dad", 1.89]
+
+# A list of lists
+family = [["liz", 1.73], ["emma", 1.68], ["mom", 1.71], ["dad", 1.89]]
+
+# Accessing elements
+print(family[0])
+print(family[1])
+print(family[2])
+print(family[3])
+
+print(type(family))
