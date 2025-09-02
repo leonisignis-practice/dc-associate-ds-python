d6d0d169c85534db7dfbf29d7535bed26322175c
leonisignis <leonisignisxii@gmail.com>
2025-09-01 23:07:22 -0500

    completed: subsetting lists

---
+++ 01-introduction-to-python/02-python-lists/05-subsetting-lists.py
@@ -0,0 +1,21 @@
+fam = ["liz", 1.73, "emma", 1.68, "mom", 1.71, "dad", 1.89]
+# zero-indexing
+print(fam[0])
+print(fam[3])
+print(fam[-1])
+print(fam[3:6])
+print(fam[0:6:2])
+
+# negative indexing
+print(fam[7])
+print(fam[-1])
+
+
+# slicing
+#    [start : end]
+# inclusive : exclusive
+print(fam[0:4])
+print(fam[:4])
+print(fam[4:])
+print(fam[:])
+print(fam[-4:-1])
