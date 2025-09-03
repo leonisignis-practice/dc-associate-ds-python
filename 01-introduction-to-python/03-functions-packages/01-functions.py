84ed2987ce83d558b5bb7d7e1827390bada1c6cf
leonisignis <leonisignisxii@gmail.com>
2025-09-03 14:09:43 -0500

    completed: functions

---
+++ 01-introduction-to-python/03-functions-packages/01-functions.py
@@ -0,0 +1,19 @@
+# Find the tallest family member's height
+fam_heights = [1.73, 1.68, 1.71, 1.89]
+max(fam_heights)
+print(max(fam_heights))
+
+# assign max height to variable
+tallest = max(fam_heights)
+print(tallest)
+
+# Find the shortest family member's height
+shortest = min(fam_heights)
+print(shortest)
+
+# Round heights to nearest integer
+rounded = round(1.68, 1)  # Round to 1 decimal place
+print(rounded)
+
+rounded = round(1.68)  # Round to nearest integer (default)
+print(rounded)
