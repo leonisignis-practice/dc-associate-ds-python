144c87fe8e34c19c636550c3b87ee0d7e2089a67
leonisignis <leonisignisxii@gmail.com>
2025-12-16 00:03:29 -0500

    completed: Loop over a Numpy array

--- 02-intermediate-python/04-loops/12-loop-over-numpy-array.py
+++ 02-intermediate-python/04-loops/12-loop-over-numpy-array.py
@@ -1,6 +1,12 @@
 # Import numpy as np
 import numpy as np
 
+height = [1.73, 1.68, 1.71, 1.89, 1.79]
+np_height = np.array(height)
+
+baseball = [[180, 78.4], [215, 102.7], [210, 98.5], [188, 75.2]]
+np_baseball = np.array(baseball)
+
 # For loop over np_height
 for height in np_height:
     print(f"{height} inches")
