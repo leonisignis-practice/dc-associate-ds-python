269f0fb6998512ca36f99571528617c51412d990
leonisignis <leonisignisxii@gmail.com>
2025-12-15 23:52:23 -0500

    completed: Looping over a dictionary

---
+++ 02-intermediate-python/04-loops/11-loop-over-dictionary.py
@@ -0,0 +1,14 @@
+# Definition of dictionary
+europe = {
+    "spain": "madrid",
+    "france": "paris",
+    "germany": "berlin",
+    "norway": "oslo",
+    "italy": "rome",
+    "poland": "warsaw",
+    "austria": "vienna",
+}
+
+# Iterate over europe
+for country, capital in europe.items():
+    print(f"the capital of {country} is {capital}")
