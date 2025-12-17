05e689b71abc17ac3a29578dff8508b08a08c14e
leonisignis <leonisignisxii@gmail.com>
2025-12-17 00:35:18 -0500

    completed: Add column (1)

---
+++ 02-intermediate-python/04-loops/16-add-column.py
@@ -0,0 +1,16 @@
+# Import cars data
+import pandas as pd
+import os
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir, "cars.csv")
+
+cars = pd.read_csv(file_path)  # this keeps the country column as data
+# cars = pd.read_csv(file_path, index_col=0)
+print(cars)
+# Code for loop that adds COUNTRY column
+for label, row in cars.iterrows():
+    cars.loc[label, "COUNTRY"] = row["country"].upper()
+
+# Print cars
+print(cars)
